5e5a312f531a812201b6e93fe51ff34fbf2d9fce
Nymphus <138739223+wells-pixel@users.noreply.github.com>
2025-12-28 07:15:42 -0500

    drivetrain, intake and fire

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/Drive/demo.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode.Libs.Drive;
+
+import static org.firstinspires.ftc.teamcode.Libs.JCLibs.lerp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Libs.PlayOpMode;
+
+@TeleOp(name = "Drive", group = "demo")
+public class demo extends PlayOpMode {
+    HardwareMap map;
+
+    //Clas Ref
+    drivetrain d = new drivetrain();
+
+    //Controller
+    double lsx;
+    double rsx;
+    double lsy;
+
+    //Variables
+    double theta;
+    double Speed1 = 0.6;
+    double Speed2 = 1;
+    double lsx_lerped = 0;
+    double rsx_lerped = 0;
+    double lsy_lerped = 0;
+    boolean Drive;
+    boolean Speed;
+    boolean a;
+    float RT;
+
+    @Override
+    protected void initialize() {
+        map = hardwareMap;
+
+        d.init(map);
+        d.initializeMotor(d.BL, DcMotor.Direction.REVERSE);
+        d.initializeMotor(d.FL, DcMotor.Direction.FORWARD);
+        d.initializeMotor(d.FR, DcMotor.Direction.REVERSE);
+        d.initializeMotor(d.BR, DcMotor.Direction.FORWARD);
+
+        d.initializeMotor(d.im, DcMotor.Direction.FORWARD);
+        d.initializeMotor(d.FireL, DcMotor.Direction.FORWARD);
+        d.initializeMotor(d.FireR, DcMotorSimple.Direction.REVERSE);
+    }
+
+    void SwitchCase(){
+        switch (Drive ? 1:0) {
+            case 0:
+                switch (Speed ? 1 : 0) {
+                    case 0://false
+                        d.move(lsx_lerped * Speed1, lsy_lerped * Speed1, rsx_lerped * Speed1);
+                        d.intake(d.im, DcMotor.Direction.FORWARD, lsy * Speed1);
+                        break;
+
+                    case 1://true
+                        d.move(lsx_lerped * Speed2, lsy_lerped * Speed2, rsx_lerped * Speed2);
+                        d.intake(d.im, DcMotor.Direction.FORWARD, lsy * Speed1);
+                        break;
+                }
+                break;
+            case 1:
+                d.move(lsx_lerped * 0, lsy_lerped * 0, rsx_lerped * 0);
+                d.intake(d.im, DcMotor.Direction.FORWARD, lsy * 0);
+                telemetry.speak("Click L3 Genius ");
+                break;
+        }
+    }
+
+    void Controller(Gamepad gamepaD){
+        lsy = gamepaD.left_stick_y;
+        lsx = -gamepaD.left_stick_x;
+        rsx = -gamepaD.right_stick_x;
+        a = gamepaD.a;
+        RT = gamepaD.right_trigger;
+    }
+
+    void Shooter(){
+        d.Fire(d.FireL, DcMotor.Direction.FORWARD, RT);
+        d.Fire(d.FireR, DcMotorSimple.Direction.REVERSE, RT);
+    }
+
+    @Override
+    protected void run(double dt) throws InterruptedException {
+        Controller(gamepad1);
+        lsx_lerped = lerp(lsx_lerped, lsx, 0.9);
+        rsx_lerped = lerp(rsx_lerped, rsx, 0.9);
+        lsy_lerped = lerp(lsy_lerped, lsy, 0.9);
+        SwitchCase();
+
+        Shooter();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/Drive/drivetrain.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Libs.Drive;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class drivetrain {
+    public DcMotor FL,FR,BL,BR,im,FireL,FireR;
+    public void init (HardwareMap map) {
+        FL = map.get(DcMotor.class, "Front Left");
+        FR = map.get(DcMotor.class, "Front Right");
+        BL = map.get(DcMotor.class, "Back Left");
+        BR = map.get(DcMotor.class, "Back Right");
+        im = map.get(DcMotor.class, "intake");
+        FireL = map.get(DcMotor.class, "FireL");
+        FireR = map.get(DcMotor.class, "FireR");
+    }
+    
+    void initializeMotor(DcMotor motor, DcMotor.Direction direction) {
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setDirection(direction);
+    }
+
+    public void move(double x, double y, double turn) {
+        FL.setPower(y+x+turn);
+        BL.setPower(y-x+turn);
+        FR.setPower(y-x-turn);
+        BR.setPower(y+x-turn);
+    }
+
+    public void intake(DcMotor motor, DcMotor.Direction direction, double power) {
+        if( power < 0){
+            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setDirection(direction);
+            motor.setPower(power);
+        } else {
+                 motor.setPower(0);
+
+        }
+
+    }
+
+    public void Fire(DcMotor motor, DcMotor.Direction direction, double power) {
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setDirection(direction);
+        motor.setPower(power);
+    }
+}
+
